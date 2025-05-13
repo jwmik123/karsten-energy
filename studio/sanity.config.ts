d8b3f9219c0ff0468717e551c9669a00b042d365
Joël Mik <contact@joelmik.nl>
2025-05-13 15:17:17 +0200

    preview url change

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -22,7 +22,8 @@ const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'your-projectID'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
 
 // URL for preview functionality, defaults to localhost:3000 if not set
-const SANITY_STUDIO_PREVIEW_URL = process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
+const SANITY_STUDIO_PREVIEW_URL =
+  process.env.SANITY_STUDIO_PREVIEW_URL || 'https://www.karstenenergy.eu'
 
 // Define the home location for the presentation tool
 const homeLocation = {
